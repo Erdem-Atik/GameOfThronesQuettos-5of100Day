63335a0d922a29766e1f858a0274737cc3b08b84
Erdem-Atik <erdematik8@gmail.com>
2022-01-10 15:32:31 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+const GoTAPI = "https://game-of-thrones-quotes.herokuapp.com/v1/random";
+const btn = document.querySelector(".check");
+const quotes = document.querySelector(".quotes");
+
+const getQuotes = async function () {
+  const res = await fetch(GoTAPI);
+  const rep = await res.json();
+  const { sentence: sen, character: char } = rep;
+  const sum = `${char.name}:" ${sen} "`;
+  return sum;
+};
+
+btn.addEventListener("click", function (e) {
+  e.preventDefault();
+  getQuotes().then((sm) => {
+    quotes.value = sm;
+    quotes.innerHTML = sm;
+  });
+});
